13cee7dc35e788b6303cf9313206819e67a4f7d2
sonic011gamer <efimgirjeu@gmail.com>
2024-10-26 20:00:52 +0300

    Update batch script.

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ A Powershell script to install Developer menu, bootshim and LK onto your Lumia
 
 ## Instructions
 -   Unlock your device with WPinternals
--   From WPinternals, reboot to mass storage mode
+-   From WPinternals, reboot to mass storage mode (you might want to make a Win32DiskImager backup)
 -   Clone this repo
--   Run install.ps1 as administrator
--   Provide the path to EFIESP (Windows might also have mounted it inside MainOS)
+-   Run install.bat
+-   Choose the path to EFIESP (Windows might also have mounted it inside MainOS)
 -   Unmount mass storage
 -   Reboot the device (keep pressed power key)
--   Device should output some print stuff then go into a black screen, you SHOULD be in LK by that point.
--   The script will boot you in TWRP, make the partitions needed for android.
+-   Device should output some print stuff then go into a black screen, you SHOULD be in LK by that point.(check device manager and install [drivers](https://developer.android.com/studio/run/win-usb))
+-   The script will boot you in TWRP, make the partitions needed for android and back up your provisioned partitions.
 -   You will reboot to the bootloader after this process is done, it will flash TWRP and reboot to it again.
 -   Then you will have to sideload the LOS rom of your choice (for now only 18.1 is available).
 -   Then continue with the script, it will reboot you to the bootloader to flash the kernel, (WIP: and modem) then boot you in android.
--- installer.bat
+++ installer.bat
@@ -1,13 +1,19 @@
 @echo off
-REM Requires Administrator Privileges
-
-REM Check if the first argument (efiesp_location) is provided
-IF "%~1"=="" (
-    echo Usage: %0 ^<efiesp_location^>
-    exit /b 1
+:: Check for admin privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    :: Relaunch the batch file with admin rights
+    PowerShell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
 )
+::Prompt for EFIESP directory
+setlocal
 
-set "efiesp_location=%~1"
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Select EFIESP directory.',0,0).self.path""
+
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "efiesp_location=%%I"
 
 REM Define paths
 set "bcd_file=%efiesp_location%\EFI\Microsoft\BOOT\BCD"
@@ -15,35 +21,22 @@ set "bootmgr_efis_location=%efiesp_location%\Windows\System32\BOOT"
 
 REM Check if BCD file exists
 IF NOT EXIST "%bcd_file%" (
-    echo BCD file not found at %bcd_file%
+    echo "BCD file not found at %bcd_file% make sure it's the right path"
     exit /b 1
 )
 
 echo Replacing BCD
 copy /y "%~dp0DATA\BCD" "%bcd_file%" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to copy BCD file
-    exit /b 1
-)
 
 echo Copying bootshim
 copy /y "%~dp0DATA\bootshim.efi" "%bootmgr_efis_location%" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to copy bootshim.efi
-    exit /b 1
-)
+
 copy /y "%~dp0DATA\Stage2.efi" "%efiesp_location%" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to copy Stage2.efi
-    exit /b 1
-)
+
 
 echo Copying developermenu
 copy /y "%~dp0DATA\developermenu.efi" "%bootmgr_efis_location%" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to copy developermenu.efi
-    exit /b 1
-)
+
 if not exist "%bootmgr_efis_location%\ui" (
     md "%bootmgr_efis_location%\ui" >nul 2>&1
     IF ERRORLEVEL 1 (
@@ -52,116 +45,79 @@ if not exist "%bootmgr_efis_location%\ui" (
     )
 )
 copy /y "%~dp0ui\*" "%bootmgr_efis_location%\ui\" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to copy ui files
-    exit /b 1
-)
+
 
 echo Copying LK2ND
 copy /y "%~dp0DATA\emmc_appsboot.mbn" "%efiesp_location%" >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to copy emmc_appsboot.mbn
-    exit /b 1
-)
 
 echo Done! Reboot your phone and you should be prompted to LK2ND!
 pause
 
-echo Booting TWRP
+echo Booting recovery
 
-REM Check if fastboot.exe is available
-where fastboot.exe >nul 2>&1 || (
-    echo fastboot.exe not found in PATH
-    exit /b 1
-)
+%~dp0bin\fastboot boot "%~dp0DATA\twrp.img"
 
-fastboot boot "%~dp0DATA\twrp.img"
-IF ERRORLEVEL 1 (
-    echo Failed to boot TWRP
-    exit /b 1
-)
-
+:waitforadb
 echo Waiting for device in recovery mode...
-adb wait-for-device
-IF ERRORLEVEL 1 (
-    echo Failed to detect device
-    exit /b 1
+timeout /t 7 /nobreak
+for /f "tokens=1" %%i in ('%~dp0bin\adb devices') do (
+    if "%%i" NEQ "List" (
+        if "%%i" NEQ "" (
+            echo ADB device connected: %%i
+            goto insiderecovery
+        )
+    )
 )
+goto waitforadb
 
+:insiderecovery
 echo Copying partition script
-adb push "%~dp0partition.sh" / >nul 2>&1
+%~dp0bin\adb push "%~dp0partition.sh" / >nul 2>&1
 IF ERRORLEVEL 1 (
     echo Failed to push partition.sh
     exit /b 1
 )
 
 echo Running partition script
-adb shell "bash /partition.sh"
+%~dp0bin\adb shell "bash /partition.sh"
 IF ERRORLEVEL 1 (
     echo Failed to run partition.sh
     exit /b 1
 )
 
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%i
+set "current_date=-%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4%_%datetime:~8,4%"
+
+rem Create the backup folder with the current date
+set "backup_folder=backup%current_date%"
+mkdir "%backup_folder%"
+
 echo Pulling backup
-adb pull /backup >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to pull backup
-    exit /b 1
-)
+%~dp0bin\adb pull /backup "%backup_folder%" >nul 2>&1
 
 echo Rebooting to bootloader
-adb reboot bootloader
-IF ERRORLEVEL 1 (
-    echo Failed to reboot to bootloader
-    exit /b 1
-)
+%~dp0bin\adb reboot bootloader
 
 REM Flashing recovery and modem images
-fastboot flash recovery "%~dp0DATA\twrp.img"
-IF ERRORLEVEL 1 (
-    echo Failed to flash recovery image
-    exit /b 1
-)
-fastboot flash modem "%~dp0DATA\modem.img"
-IF ERRORLEVEL 1 (
-    echo Failed to flash modem image
-    exit /b 1
-)
+%~dp0bin\fastboot flash recovery "%~dp0DATA\twrp.img"
+
+%~dp0bin\fastboot flash modem "%~dp0DATA\modem.img"
 
 echo Rebooting to recovery
-fastboot reboot recovery
-IF ERRORLEVEL 1 (
-    echo Failed to reboot to recovery
-    exit /b 1
-)
+%~dp0bin\fastboot reboot recovery
 
-adb wait-for-device
-IF ERRORLEVEL 1 (
-    echo Failed to detect device in recovery mode
-    exit /b 1
-)
+timeout /t 7 /nobreak
+
 
 echo Copying provisioning script
-adb push "%~dp0provision.sh" / >nul 2>&1
-IF ERRORLEVEL 1 (
-    echo Failed to push provision.sh
-    exit /b 1
-)
+%~dp0bin\adb push "%~dp0provision.sh" / >nul 2>&1
 
 echo Running provisioning script
-adb shell "bash /provision.sh"
-IF ERRORLEVEL 1 (
-    echo Failed to run provision.sh
-    exit /b 1
-)
+%~dp0bin\adb shell "bash /provision.sh"
 
 echo Sideload an Android ROM then press any key.
 pause
 
-adb reboot
-IF ERRORLEVEL 1 (
-    echo Failed to reboot device
-    exit /b 1
-)
+%~dp0bin\adb reboot
 
 echo Booting Android...
